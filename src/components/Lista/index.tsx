8646a339412e467292cb8b1e250c040590364581
brunoholanda <holanda_rodrigues@hotmail.com>
2023-03-04 18:48:03 -0300

    fazendo o javascript pegar informacoes do html

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,18 +1,10 @@
+import { Itarefa } from '../../types/tarefa';
 import Item from './Item';
 import style from './Lista.module.scss';
 
-function Lista() {
 
-    const tarefas = [{
-        tarefa: 'React',
-        tempo: '02:00:00'
-    }, {
-        tarefa: 'Javascript',
-        tempo: '01:00:00'
-    }, {
-        tarefa: 'Typerscript',
-        tempo: '03:00:00'
-    }]
+function Lista({ tarefas }: { tarefas: Itarefa[]}) {
+   
     return (
         <aside className={style.listaTarefas}>
             <h2> Estudos do dia </h2>
--- src/components/pages/App.tsx
+++ src/components/pages/App.tsx
@@ -2,12 +2,18 @@ import Cronometro from "../Cronometro";
 import Formulario from "../Formulario";
 import Lista from "../Lista";
 import style from './App.module.scss';
+import React, { useState } from 'react';
+import { Itarefa } from "../../types/tarefa";
 
 function App() {
+
+  const [tarefas, setTarefas] = useState<Itarefa[] | []>([]);
+
+
   return (
     <div className={style.AppStyle}>
-      <Formulario />
-      <Lista />
+      <Formulario setTarefas={setTarefas} />
+      <Lista tarefas={tarefas} />
       <Cronometro />
     </div>
   );
