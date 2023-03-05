0fc922335fdc9d78b81397e508b6d6dc25d06221
brunoholanda <holanda_rodrigues@hotmail.com>
2023-03-05 20:02:56 -0300

    a loucura do typerscript

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -1,13 +1,34 @@
 import Botao from "../Botao";
 import Relogio from "./Relogio";
 import style from './Cronometro.module.scss';
+import { Itarefa } from "../../types/tarefa";
+import { useState, useEffect } from 'react';
+import { tempoParaSegundos } from "../../common/utils/time";
 
-export default function Cronometro() {
+interface Props {
+    selecionado: Itarefa | undefined
+}
+
+export default function Cronometro({ selecionado }: Props) {
+    const [tempo, setTempo] = useState<number>();
+
+    useEffect(() => {
+        if(selecionado?.tempo) {
+         setTempo(tempoParaSegundos(selecionado.tempo));
+        }
+      }, [selecionado])
+      
+
     return (
         <div className={style.cronometro}>
-            <p className={style.titulo}> Escolha um card e inicie o cronômetro </p>
+            <p className={style.titulo}> 
+            Escolha um card e inicie o cronômetro 
+            </p>
+            Tempo: {tempo}
             <div className={style.relogioWrapper}>
-                <Relogio />
+                <Relogio 
+                    tempo={tempo}
+                />
             </div>
             <Botao>
                 Comecar
--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -2,16 +2,21 @@ import { Itarefa } from '../../types/tarefa';
 import Item from './Item';
 import style from './Lista.module.scss';
 
+interface Props {
+    tarefas: Itarefa[],
+    selecionaTarefa: (tarefaSelecionada: Itarefa) => void
+}
 
-function Lista({ tarefas }: { tarefas: Itarefa[]}) {
+function Lista({ tarefas, selecionaTarefa }: Props) {
    
     return (
         <aside className={style.listaTarefas}>
             <h2> Estudos do dia </h2>
             <ul>
-                {tarefas.map((item, index) => (
+                {tarefas.map((item) => (
                     <Item 
-                        key={index}
+                        selecionaTarefa={selecionaTarefa}
+                        key={item.id}
                         {...item}
                     /> //assim eu utilizei todos os itens que estao dentro do item tsx
                 ))}
